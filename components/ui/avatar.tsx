67c5e30a9b948ced06813c8b52e20ee3c9b33612
Newtaplix <newton4raul@gmail.com>
2025-08-25 23:38:11 +0100

    pics

--- components/ui/avatar.tsx
+++ components/ui/avatar.tsx
@@ -8,23 +8,23 @@ export const Avatar = () => {
  
     const people = [
         {
-            image: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=150&h=150&fit=crop&crop=face",
+            image: "/pic2.jpg",
             name: "Chen",
         },
         {
-            image: "https://images.unsplash.com/photo-1494790108755-2616b612b786?w=150&h=150&fit=crop&crop=face",
+            image: "/pic5.jpg",
             name: "Johnson",
         },
         {
-            image: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=150&h=150&fit=crop&crop=face",
+            image: "/pic3.jpg",
             name: "Michael",
         },
         {
-            image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=150&h=150&fit=crop&crop=face",
+            image: "/pic4.jpg",
             name: "Wilson",
         },
         {
-            image: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=150&h=150&fit=crop&crop=face",
+            image: "/pic.jpg",
             name: "David",
         }
     ]
